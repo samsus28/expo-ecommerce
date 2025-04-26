4d9ca694f67b1ceefe905b5071ea8de85dc541a6
samsus28 <alphadevel0per76@gmail.com>
2025-04-26 17:44:22 +0300

    admin routes added

---
+++ backend/src/routes/admin.route.js
@@ -0,0 +1,33 @@
+import { Router } from "express";
+import {
+  createProduct,
+  getAllCustomers,
+  getAllOrders,
+  getAllProducts,
+  getDashboardStats,
+  updateOrderStatus,
+  updateProduct,
+} from "../controllers/admin.controller.js";
+import { adminOnly, protectRoute } from "../middleware/auth.middleware.js";
+import { upload } from "../middleware/multer.middleware.js";
+
+const router = Router();
+
+// optimization - DRY
+router.use(protectRoute, adminOnly);
+
+router.post("/products", upload.array("images", 3), createProduct);
+router.get("/products", getAllProducts);
+router.put("/products/:id", upload.array("images", 3), updateProduct);
+
+router.get("/orders", getAllOrders);
+router.patch("/orders/:orderId/status", updateOrderStatus);
+
+router.get("/custormers", getAllCustomers);
+
+router.get("/stats", getDashboardStats);
+
+// PUT: Used for full resource replacement, updating the entire resource
+// PATCH: Used for partial resource updates, updating a specific part of the resource
+
+export default router;
--- backend/src/server.js
+++ backend/src/server.js
@@ -8,6 +8,8 @@ import { functions, inngest } from "./config/inngest.js";
 import { ENV } from "./config/env.js";
 import { connectDB } from "./config/db.js";
 
+import adminRoutes from "./routes/admin.route.js";
+
 const app = express();
 
 const __dirname = path.resolve();
@@ -16,6 +18,8 @@ app.use(express.json());
 app.use(clerkMiddleware()); // adds auth object under the req => req.auth
 
 app.use("/api/inngest", serve({ client: inngest, functions }));
+
+app.use("/api/admin", adminRoutes);
 
 app.get("/api/health", (req, res) => {
   res.status(200).json({ message: "Success" });
