c79106590d3e9c72acbaef93dd5c7b4d01fe36a1
samsus28 <alphadevel0per76@gmail.com>
2025-05-05 19:43:49 +0300

    order routes & controllers added (server.js)

--- backend/src/server.js
+++ backend/src/server.js
@@ -10,6 +10,7 @@ import { connectDB } from "./config/db.js";
 
 import adminRoutes from "./routes/admin.route.js";
 import userRoutes from "./routes/user.route.js";
+import orderRoutes from "./routes/order.route.js";
 
 const app = express();
 
@@ -22,6 +23,7 @@ app.use("/api/inngest", serve({ client: inngest, functions }));
 
 app.use("/api/admin", adminRoutes);
 app.use("/api/users", userRoutes);
+app.use("/api/orders", orderRoutes);
 
 app.get("/api/health", (req, res) => {
   res.status(200).json({ message: "Success" });
