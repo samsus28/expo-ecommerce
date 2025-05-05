1e8df9a120da826ebcc9fdb5e99e7b0a17e33f40
samsus28 <alphadevel0per76@gmail.com>
2025-05-05 12:04:42 +0300

    tiny fixes in review.controller.js (product.route.js)

--- backend/src/routes/product.route.js
+++ backend/src/routes/product.route.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { protectRoute } from "../middleware/auth.middleware.js";
-import { getAllProducts } from "../controllers/admin.controller";
+import { getAllProducts } from "../controllers/admin.controller.js";
 import { getProductById } from "../controllers/product.controller.js";
 
 const router = Router();
