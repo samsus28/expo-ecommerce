890133d2f12fa4fa54baaf8d33e38aefd18ba1c7
samsus28 <alphadevel0per76@gmail.com>
2025-05-06 23:21:39 +0300

    tiny fix in cart.controller.js

--- backend/src/controllers/cart.controller.js
+++ backend/src/controllers/cart.controller.js
@@ -39,6 +39,8 @@ export async function addToCart(req, res) {
     let cart = await Cart.findOne({ clerkId: req.user.clerkId });
 
     if (!cart) {
+      const user = req.user;
+
       cart = await Cart.create({
         user: user._id,
         clerkId: user.clerkId,
