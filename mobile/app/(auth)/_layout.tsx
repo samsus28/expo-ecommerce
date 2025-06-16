168e892439c5d377c2344589677ea98a272c1cf6
samsus28 <alphadevel0per76@gmail.com>
2025-06-16 20:26:29 +0300

    tabs navigator and shop screen added

--- mobile/app/(auth)/_layout.tsx
+++ mobile/app/(auth)/_layout.tsx
@@ -2,11 +2,13 @@ import { Redirect, Stack } from "expo-router";
 import { useAuth } from "@clerk/clerk-expo";
 
 export default function AuthRoutesLayout() {
-  const { isSignedIn } = useAuth();
+  const { isSignedIn, isLoaded } = useAuth();
 
-  // if (isSignedIn) {
-  //   return <Redirect href={"/"} />;
-  // }
+  if (!isLoaded) return null; // for a better ux
+
+  if (isSignedIn) {
+    return <Redirect href={"/(tabs)"} />;
+  }
 
   return <Stack screenOptions={{ headerShown: false }} />;
 }
