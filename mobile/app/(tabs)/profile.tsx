ca7bcc706807a919a94f34866f7c4441cdb852a5
samsus28 <alphadevel0per76@gmail.com>
2025-09-05 22:08:31 +0300

    wishlist screen added (profile.tsx)

--- mobile/app/(tabs)/profile.tsx
+++ mobile/app/(tabs)/profile.tsx
@@ -4,6 +4,7 @@ import { useAuth, useUser } from "@clerk/clerk-expo";
 import { ScrollView, Text, TouchableOpacity, View } from "react-native";
 import { Image } from "expo-image";
 import { Ionicons } from "@expo/vector-icons";
+import { router } from "expo-router";
 
 const MENU_ITEMS = [
   { id: 1, icon: "person-outline", title: "Edit Profile", color: "#3B82F6", action: "/profile" },
@@ -18,7 +19,7 @@ const ProfileScreen = () => {
 
   const handleMenuPress = (action: (typeof MENU_ITEMS)[number]["action"]) => {
     if (action === "/profile") return;
-    // router.push(action)
+    router.push(action);
   };
 
   return (
@@ -95,7 +96,7 @@ const ProfileScreen = () => {
           <TouchableOpacity
             className="flex-row items-center justify-between py-2"
             activeOpacity={0.7}
-            // onPress={() => router.push("/privacy-security")}
+            onPress={() => router.push("/privacy-security")}
           >
             <View className="flex-row items-center">
               <Ionicons name="shield-checkmark-outline" size={22} color="#FFFFFF" />
