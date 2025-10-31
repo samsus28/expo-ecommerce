b769ced4b258e4f5f1c58a26ad957901db2626fb
samsus28 <alphadevel0per76@gmail.com>
2025-10-31 13:45:09 +0200

    orders screen added (useCart.ts, index.ts etc)

--- mobile/hooks/useCart.ts
+++ mobile/hooks/useCart.ts
@@ -2,7 +2,6 @@ import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { useApi } from "@/lib/api";
 import { Cart } from "@/types";
 
-// todo: complete this hook now!
 const useCart = () => {
   const api = useApi();
   const queryClient = useQueryClient();
@@ -65,7 +64,7 @@ const useCart = () => {
     addToCart: addToCartMutation.mutate,
     updateQuantity: updateQuantityMutation.mutate,
     removeFromCart: removeFromCartMutation.mutate,
-    clearCart: clearCartMutation.mutateAsync,
+    clearCart: clearCartMutation.mutate,
     isAddingToCart: addToCartMutation.isPending,
     isUpdating: updateQuantityMutation.isPending,
     isRemoving: removeFromCartMutation.isPending,
